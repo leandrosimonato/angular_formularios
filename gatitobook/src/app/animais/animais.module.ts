83dc644847b300af1c1689e05c931227be249d95
Leandro Simonato <leandrosimonato@gmail.com>
2022-01-11 16:47:52 -0300

    Grade de fotos dos animais.

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
+import { CartaoModule } from './../componentes/cartao/cartao.module';
+import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent],
+  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent],
   imports: [
-    CommonModule,
-    AnimaisRoutingModule
+
+  CommonModule,
+    AnimaisRoutingModule,
+    CartaoModule
   ]
 })
 export class AnimaisModule { }
