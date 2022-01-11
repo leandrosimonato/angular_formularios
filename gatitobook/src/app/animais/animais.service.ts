e2997cbf7a7e3b7db2bf8e2d65cebb44d23d0a41
Leandro Simonato <leandrosimonato@gmail.com>
2022-01-11 16:31:18 -0300

    Environment.

---
+++ gatitobook/src/app/animais/animais.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Animais } from './animais';
+import { TokenService } from './../autenticacao/token.service';
+import { environment } from 'src/environments/environment';
+
+const API = environment.apiURL;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimaisService {
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
+
+  listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
+      headers,
+    });
+  }
+}
