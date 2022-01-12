e64dfae846b28753ff0a3f3a91d4139d35d62d99
Leandro Simonato <leandrosimonato@gmail.com>
2022-01-12 10:56:43 -0300

    Componente de detalhe.

--- gatitobook/src/app/animais/animais-routing.module.ts
+++ gatitobook/src/app/animais/animais-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListaAnimaisComponent,
   },
+  {
+    path: ':animalId',
+    component: DetalheAnimalComponent,
+  }
 ];
 
 @NgModule({
--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -6,15 +6,17 @@ import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
 import { CartaoModule } from './../componentes/cartao/cartao.module';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent],
+  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent],
   imports: [
 
   CommonModule,
     AnimaisRoutingModule,
     CartaoModule
-  ]
+  ],
+  exports: []
 })
 export class AnimaisModule { }
